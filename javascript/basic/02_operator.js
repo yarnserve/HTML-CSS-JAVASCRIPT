2fcda3519495a4fe549a14177f181cba313c1f73
yarnserve <yarnserve@gmail.com>
2023-10-19 14:15:17 +0900

    init

---
+++ javascript/basic/02_operator.js
@@ -0,0 +1,69 @@
+// 1. 산술 연산자
+{
+    // console.log(100 + 10)
+    // console.log(100 - 10)
+    // console.log(100 * 10)
+    // console.log(100 / 10)
+    // console.log(100 % 10)    
+}
+
+// 2. 문자열 연결 연산자
+{
+    // console.log('Winter'+ ' ' + 'is' + ' ' + 'coming')
+    // const say1 = 'Winter'
+    // const say2 = 'is coming'
+    // console.log(`${say1} ${say2}`)
+    // console.log('10' + 2)
+}
+
+// 3. 대입 연산자
+{
+    // let x = 10
+    // let y = 5
+    // x = x + y // x += y
+    // console.log(x) // 15
+
+    // x = x - y // x -= y
+    // console.log(x) // 10
+
+    // x = x * y // x *= y
+    // console.log(x) // 50
+
+    // x = x / y // x /= y
+    // console.log(x) // 10
+    
+    // x = x % y // x %= y
+    // console.log(x) // 0
+
+    // let tbl = '<table border="1" class="tbl">'
+    // tbl += '<tr>'
+    // tbl += '<td>1</td><td>2</td><td>3</td>'
+    // tbl += '</tr>'
+    // tbl += '</table>'
+
+    // document.write(tbl)
+}
+
+// 4. 증감 연산자 ++ --
+{
+    // let number = 8
+    // console.log(number)
+    // number++
+    // console.log(number)
+    // number--
+    // console.log(number)
+
+    // let num1 = 10
+    // let num2 = ++num1
+    // console.log(num1, num2)
+}
+
+// 5. 비교 연산자
+{
+    console.log(10 > 100)
+    console.log(10 < 100)
+    console.log(10 >= 100)
+    console.log(10 <= 100)
+    console.log(10 == 100)
+    console.log(10 != 100)
+}
