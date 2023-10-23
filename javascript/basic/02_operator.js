1fc6128fb8449f27a7c68c8a653c7094d60d04c2
yarnserve <yarnserve@gmail.com>
2023-10-23 14:15:15 +0900

    init

--- javascript/basic/02_operator.js
+++ javascript/basic/02_operator.js
@@ -60,10 +60,74 @@
 
 // 5. 비교 연산자
 {
-    console.log(10 > 100)
-    console.log(10 < 100)
-    console.log(10 >= 100)
-    console.log(10 <= 100)
-    console.log(10 == 100)
-    console.log(10 != 100)
+    // console.log(10 > 100)
+    // console.log(10 < 100)
+    // console.log(10 >= 100)
+    // console.log(10 <= 100)
+    // console.log(10 == 100)
+    // console.log(10 != 100)
+}
+
+// 6. 논리 연산자
+// ||(or), &&(and), !(not)
+{
+    // let a = 100 > 50
+    // let b = 10 < 5
+    // let c = true
+
+    // // || => 한개라도 true이면 -> ture
+    // console.log( a || b || c)
+
+    // // && => 전부 true일때 만 -> ture
+    // console.log( a && b && c)
+
+    // // ! => 값을 반대로 바꾼다.
+    // b = !b
+    // console.log(b)
+    // console.log( a && b && c)
+}
+
+// 7. 동등 / 일치 연산자
+// ==, ===, !=, !==
+{
+    // const value1 = '100'
+    // const value2 = 100
+    // console.log(value1 == value2)
+    // console.log(value1 != value2)
+    // console.log(value1 === value2)
+    // console.log(value1 !== value2)
+    // console.log('===============')
+    // console.log( 0 == false)
+    // console.log( 0 === false)
+    // console.log( '' == false)
+    // console.log( '' === false)
+    // console.log(null == undefined)
+    // console.log(null === undefined)
+}
+
+// 8. 삼항 조건 연산자
+{
+    // let a = 20
+    // let b = 15
+    // const result = a > b ? '감사합니다.' : '안녕하세요'
+    // console.log(result)
+}
+
+// 실습 : 적정 체중 테스트기
+// 홍길동 : 키-180cm, 체중-80kg
+// 적정체중 : (신장 - 100) * 0.9
+{
+    // const userHeight = 180
+    // const userWeight = 80
+    // const normalWeight = (userHeight - 100) * 0.9
+    // console.log(normalWeight)
+
+    // const name = prompt('당신의 이름은?')
+    // const height = prompt('당신의 키는?')
+    // const weight = prompt('당신의 체중은?')
+    // const normalWeight = (height - 100) * 0.9
+    // const result = weight >= normalWeight -5 && weight <= normalWeight + 5
+    // const message = result ? '적정 체중입니다.' : '적정체중이 아닙니다.'
+
+    // document.write(`${name}님은 ${message}`)
 }
