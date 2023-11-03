054d76c41efd032013a782625525ae77115b4afa
yarnserve <yarnserve@gmail.com>
2023-11-03 14:30:05 +0900

    init

---
+++ javascript/basic/09_object_string.js
@@ -0,0 +1,57 @@
+// String Object 문자열 객체
+{
+    // const str = 'you he she they we he me'
+    // console.log(str.length)
+    // console.log(str.charAt(4)) // 인덱스에 해당하는 문자 반환
+    // console.log(str.indexOf('he')) // 제일 먼저 일치 -> 인덱스 반환
+    // console.log(str.lastIndexOf('he')) // 오른쪽에서...
+    // console.log(str.lastIndexOf('cat')) // 없으면 -1 반환
+    // console.log(str.replace('they', 'html')) // 교체
+    // console.log(str.search('she')) // 인덱스 반환
+    // console.log(str.slice(4, 10)) // a에서 b까지 반환
+}
+
+// 문자열을 배열로 만들기
+{
+    // const str = 'html,css,javascript'
+    // const result = str.split(',')
+    // console.log(result);
+}
+
+// 대소문자
+{
+    // const str = 'hello'
+    // const str2 = 'WORLD'
+    // console.log(str.toUpperCase())
+    // console.log(str2.toLocaleLowerCase())
+}
+
+// 문자열 합치기
+{
+    // const str = 'hello'
+    // const result = str.concat(' world')
+    // console.log(result);
+}
+
+// 공백 제거
+{
+    // const str = ' hello world'
+    // const result = str.trim()
+    // console.log(str)
+    // console.log(result)
+}
+
+// 방문자의 이름을 대문자로 출력
+// 전화번호의 뒷자리 4개를 *로 출력
+{
+    // const name = prompt('당신의 이름은?', '')
+    // const phone = prompt('당신의 전화번호는?', '')    
+
+    // const upperName = name.toUpperCase()
+    // const result = phone.substring(0, phone.length - 4) + '****'
+
+    // document.write(upperName, '<br>')
+    // document.write(result, '<br>')
+}
+
+
---
+++ javascript/basic/10_object_BOM.js
@@ -0,0 +1,30 @@
+// Browser Object Model
+{
+    // window.open('https://daum.net')
+    // window.alert('안녕')
+    // window.prompt('당신의 전화번호는', '010-')
+    // window.confirm('정말 삭제하시겠습니까?')
+    // window.setInterval(function(){
+    //     console.log('hello')
+    // }, 1000)
+
+    // window.setTimeout(function(){
+    //     console.log('world')
+    // }, 3000)
+}
+
+// 스크린 개체
+{
+    console.log(screen.width)
+    console.log(screen.height)
+}
+
+// location 객체 
+{
+    console.log(location.href)
+    console.log(location.hash)
+    console.log(location.hostname)
+    console.log(location.host)
+    console.log(location.protocol)
+    console.log(location.search) // url 쿼리요청값을 반환
+}
