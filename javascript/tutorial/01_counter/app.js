e351bc1249eb1af726830279b35725fd496b5334
yarnserve <yarnserve@gmail.com>
2023-11-06 11:10:05 +0900

    init

--- javascript/tutorial/01_counter/app.js
+++ javascript/tutorial/01_counter/app.js
@@ -6,10 +6,27 @@ const totalPrice = document.querySelector('.total span')
 
 const unit_price = 30000
 let num = 1
-onePrice.textContent = unit_price 
+onePrice.textContent = unit_price.toLocaleString() 
 count.textContent = num
 
 plus.addEventListener('click', function(){
     num++
     count.textContent = num
+    paintTotal()
 })
+
+minus.addEventListener('click', function(){
+    num--
+    if( num < 1) {
+        alert('최소 주문 수량은 한개 입니다.')
+        num = 1
+    }
+    count.textContent = num
+    paintTotal()
+})
+
+function paintTotal() {
+    totalPrice.textContent = (unit_price * num).toLocaleString()
+}
+
+paintTotal()
